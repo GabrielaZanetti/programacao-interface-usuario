da08d8911260b43f8add2860d98fef94d81690af
GabrielaZanetti <gabiisctt@gmail.com>
2024-09-11 20:59:43 -0300

    ajuste do class s

--- html-css-js/script.js
+++ html-css-js/script.js
@@ -1,17 +1,23 @@
 const img = document.getElementById('img-gatinho');
+const element = document.getElementById('clique');
 
-ClickBtn ();
+ClickBtn()
+
 async function ClickBtn () {
+    element.disabled = true;
     img.setAttribute('src', 'https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif')
+    
     await fetch('https://api.thecatapi.com/v1/images/search') .then((response) => {
         return response.json();
-      })
-      .then((res) => {
+    })
+    .then((res) => {
         const {url} = res[0];
         img.setAttribute('src', url)
-      })
+        setTimeout(() => {
+            element.disabled = false;
+        }, 800)
+    })
 }
 
-const element = document.getElementById('clique');
 
 element.addEventListener("click", ClickBtn)
