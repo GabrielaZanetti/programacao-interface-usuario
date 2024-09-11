8a6c99fdad70ff9efda220286562850ddc6eb117
GabrielaZanetti <gabiisctt@gmail.com>
2024-09-11 20:40:22 -0300

    busca das img de gatinhos por api usando fetch

--- html-css-js/script.js
+++ html-css-js/script.js
@@ -1,6 +1,15 @@
+const img = document.getElementById('img-gatinho');
 
-function ClickBtn () {
-    alert('Você clicou no botão')
+ClickBtn ();
+async function ClickBtn () {
+    img.setAttribute('src', 'https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif')
+    await fetch('https://api.thecatapi.com/v1/images/search') .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        const {url} = res[0];
+        img.setAttribute('src', url)
+      })
 }
 
 const element = document.getElementById('clique');
