9da79655d1b4d0212ef521420056304f00f3695d
tiago <tiagopinzgraffunder@gmail.com>
2024-11-02 17:36:30 -0300

    Revert "feat: menu superior"
    This reverts commit 50b79f8b04cd043c42c12ad576496fbda5a366ec.

--- teste/src/main/java/com/example/teste/HelloApplication.java
+++ teste/src/main/java/com/example/teste/HelloApplication.java
@@ -10,11 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/teste/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(getClass().getResource("/com/example/teste/styles.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
