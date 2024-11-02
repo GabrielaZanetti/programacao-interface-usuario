50b79f8b04cd043c42c12ad576496fbda5a366ec
tiago <tiagopinzgraffunder@gmail.com>
2024-11-02 16:22:24 -0300

    feat: menu superior

--- teste/src/main/java/com/example/teste/HelloApplication.java
+++ teste/src/main/java/com/example/teste/HelloApplication.java
@@ -10,8 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/example/teste/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(getClass().getResource("/com/example/teste/styles.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
