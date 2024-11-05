09abde926a3dabad27e9bbb07cd723ce0a544339
GabrielaZanetti <gabiisctt@gmail.com>
2024-11-05 09:11:06 -0300

    criacao da class Produto

---
+++ teste/src/main/java/com/example/teste/Produto.java
@@ -0,0 +1,31 @@
+package com.example.teste;
+
+public class Produto {
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, String descricao, double preco, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
