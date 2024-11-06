33f1a57c0fea3c906e0f8e77da0296a669ee2b95
GabrielaZanetti <gabiisctt@gmail.com>
2024-11-06 20:22:01 -0300

    remocao de codigo

--- teste/src/main/java/com/example/teste/HelloApplication.java
+++ teste/src/main/java/com/example/teste/HelloApplication.java
@@ -24,21 +24,4 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
-
-//    public static void createTable() {
-//        String sql = "CREATE TABLE IF NOT EXISTS usuarios (\n"
-//                + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-//                + " nome TEXT NOT NULL,\n"
-//                + " email TEXT NOT NULL\n"
-//                + ");";
-//
-////        try (Connection conn = HelloController.connect();
-////             Statement stmt = conn.createStatement()) {
-////            stmt.execute(sql);
-////            System.out.println("Tabela 'usuarios' criada.");
-////        } catch (Exception e) {
-////            System.out.println(e.getMessage());
-////        }
-//    }
-
 }
